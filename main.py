cd1116282c71246bf53ca523b18af39f25bce7c5
leo <leokostarev@mail.ru>
2021-11-11 21:37:27 +0300

    Really done task 3.

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
 from PyQt5 import uic
 import random
+
+from PyQt5.QtCore import QCoreApplication, QMetaObject, QRect, QSize
 from PyQt5.QtGui import QColor, QPainter
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget
 import sys
 
 
-class Ex(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(700, 700)
+        MainWindow.setMinimumSize(QSize(700, 700))
+        MainWindow.setMaximumSize(QSize(700, 700))
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.pb = QPushButton(self.centralwidget)
+        self.pb.setObjectName(u"pb")
+        self.pb.setGeometry(QRect(320, 320, 60, 60))
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.pb.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+    # retranslateUi
+
+
+class Ex(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('ui.ui', self)
+        Ui_MainWindow.setupUi(self, self)
         self.pb.clicked.connect(self.run)
         self.circles = []
 
